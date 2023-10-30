4441c1aa748593eab95fd2f4aa4371777c9b3eab
redplusblue <samirkabra1161@gmail.com>
2023-10-30 09:48:11 -0400

    Conditional rendering with 3s loading time.

--- react/react-app/src/App.jsx
+++ react/react-app/src/App.jsx
@@ -1,28 +1,44 @@
 import './App.css'
 import PropTypes from 'prop-types'
+import React from 'react'
 // import { Greeting } from './greeting'
 
-// Even Numbers only
-function NumberList(props) {
-  const numbers = props.numbers
-  const listItems = numbers.map((number) => {
-    // Can use ternary if statement or && operator
-    return number % 2 == 0 && <li key={number.toString()}>{number}</li>
-  })
-  return <ul>{listItems}</ul>
+function List(props) {
+  if (!props.numbers) {
+    return <div>Loading...</div>;
+  }
+
+  if (props.numbers.length === 0) {
+    return <div>No numbers</div>;
+  }
+
+  return (
+    <ul>
+      {props.numbers.map((number) => (
+        <li key={number}>{number}</li>
+      ))}
+    </ul>
+  );
 }
-NumberList.propTypes = {
+List.propTypes = {
   numbers: PropTypes.arrayOf(PropTypes.number),
 }
 
 
 function App() {
-  const numbers = [...Array(200).keys()]
+  // const numbers = [...Array(200).keys()]
+  const [numbers, setNumbers] = React.useState(null);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setNumbers([...Array(200).keys()]);
+    }, 3000);
+  }, []);
   return (
     // Multiple elements
     <div>
       <h1>Numbers!</h1>
-      <NumberList numbers={numbers} />
+      <List numbers={numbers} />
     </div>
   )
 }
