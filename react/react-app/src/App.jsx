2399597982e7a3dfaaf080d3f4938091cea5ee71
redplusblue <samirkabra1161@gmail.com>
2023-10-30 09:32:52 -0400

    Using Props

--- react/react-app/src/App.jsx
+++ react/react-app/src/App.jsx
@@ -1,14 +1,32 @@
 import './App.css'
+import PropTypes from 'prop-types'
 // import { Greeting } from './greeting'
+
+function ListItem(props) {
+  return <li>{props.value}</li>
+}
+ListItem.propTypes = {
+  value: PropTypes.number,
+}
+
+function NumberList(props) {
+  const numbers = props.numbers
+  const listItems = numbers.map((number) => (
+    <ListItem key={number.toString()} value={number} />
+  ))
+  return <ul>{listItems}</ul>
+}
+NumberList.propTypes = {
+  numbers: PropTypes.arrayOf(PropTypes.number),
+}
 
 function App() {
   const numbers = [...Array(20).keys()]
-  const listItems = numbers.map((number) => <li>{number}</li>)
   return (
     // Multiple elements
     <div>
       <h1>Numbers!</h1>
-      <ul>{listItems}</ul>
+      <NumberList numbers={numbers} />
     </div>
   )
 }
